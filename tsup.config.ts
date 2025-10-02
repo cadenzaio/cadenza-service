596377a1fbf8e2459a42e488801912875bd888b4
emilforsvall <emil@canaray.com>
2025-10-02 22:22:29 +0200

    fix: build fix

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,8 +3,15 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   entry: ["src/index.ts"],
   format: ["esm", "cjs"],
-  dts: true,
+  dts: {
+    entry: "src/index.ts",
+    compilerOptions: {
+      module: "NodeNext",
+      moduleResolution: "NodeNext",
+    },
+  },
   sourcemap: true,
   clean: true,
   outDir: "dist",
+  tsconfig: "./tsconfig.json", // Explicitly use tsconfig.json
 });
